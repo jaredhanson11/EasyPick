b4703669dc436987f43014120742b9ed5b0fe43c
Jared Hanson <hansonj@mit.edu>
2016-11-19 15:36:21 -0500

    Fixed schema-setup bugs

--- models/course.js
+++ models/course.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 
 courseSchema = mongoose.Schema({
     name: {type: String},
-    course_numbers: [{type: Number}],
-    department: {type: Number},
+    course_numbers: [{type: String}],
+    department: [{type: Number}],
     units: {type: String},
     prereqs: [{type: String}],
     tags: [{type: mongoose.Schema.ObjectId, ref: 'Tag'}]
--- models/offering.js
+++ models/offering.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 offeringSchema = mongoose.Schema ({
     term: {type: String, required: true},
     year: {type: Number, required: true},
-    course: {type: mongoose.Schema.ObjectId, ref: 'User'}
+    course: {type: mongoose.Schema.ObjectId, ref: 'Course'}
 });
 
 module.exports = mongoose.model('Offering', offeringSchema);
