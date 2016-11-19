86f369817ab3a4715c3e5dc69e2d899dfaa8eaff
Jared Hanson <hansonj@mit.edu>
2016-11-19 14:51:14 -0500

    Added initial Offering model schema.

---
+++ models/offering.js
@@ -0,0 +1,11 @@
+var express = require('express'); //Do I need this import statement
+var mongoose = require('mongoose');
+
+offeringSchema = mongoose.Schema ({
+    term: {type: String, required: true},
+    year: {type: Number, required: true},
+    course: {type: mongoose.Schema.ObjectId, ref: 'User'}
+});
+
+module.exports = mongoose.model('Offering', offeringSchema);
+
