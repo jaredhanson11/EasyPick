39be32f68374d3cb563099118d65ef31c531ff46
Jared Hanson <hansonj@mit.edu>
2016-11-22 15:30:08 -0500

    small changes for PR

--- public/js/index.js
+++ public/js/index.js
@@ -46,18 +46,3 @@ var navbar_html = Handlebars.templates['nav']();
 $('.navigation').html(navbar_html);
 };
 
-var getUrlParameter = function getUrlParameter(sParam) {
-var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-  sURLVariables = sPageURL.split('&'),
-  sParameterName,
-  i;
-
-  for (i = 0; i < sURLVariables.length; i++) {
-    sParameterName = sURLVariables[i].split('=');
-
-    if (sParameterName[0] === sParam) {
-      return sParameterName[1] === undefined ? true : sParameterName[1];
-    }
-  }
-};
-
