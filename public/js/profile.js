1bf549cf04d0113b9a603235aa03a0b224e970cc
Jared Hanson <hansonj@mit.edu>
2016-11-22 18:14:18 -0500

    upated ajax call

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,22 +1,27 @@
 $(function() {
     var populate_profile = function(){
         $.get('/users', function(resp) {
-            console.log(resp.msg);
             if (resp.success) {
                 var html = Handlebars.templates['profile'](resp.msg);
                 $('.profile').html(html);
                 $('.btn#edit-user').click(function(){
-                    console.log($('input.major1'));
-                    var updatedDataString = "major1=" + $("input#major1").val() + "&minor=" +
-                    $("input#minor").val() + "&graduation_year=" + $("input#graduation_year").val() +
-                    "&first_name=" + $("input#first_name").val() + "&last_name=" + $("input#last_name").val();
+                    var updatedData = {
+                        major1: $("input#major1").val(),
+                        minor: $("input#minor").val(),
+                        graduation_year: $("input#graduation_year").val(),
+                        first_name: $("input#first_name").val(),
+                        last_name: $("input#last_name").val(),
+                    };
 
                     $.ajax({
                         url: '/users',
                         type: 'PUT',
-                        data: updatedDataString,
+                        data: JSON.stringify(updatedData),
+                        dataType: 'json',
                         success: function(data) {
                         //or refresh
+                            console.log(data);
+                            alert('Successfully updated profile!');
                             populate_profile();
                         }
                     });
