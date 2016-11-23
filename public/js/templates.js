4fb979090d8ff7c2520d6cc6f709103ddaa96d65
Jared Hanson <hansonj@mit.edu>
2016-11-23 06:24:42 -0500

    Link from recommendations page to courses page

--- public/js/templates.js
+++ public/js/templates.js
@@ -97,15 +97,17 @@ templates['profile'] = template({"1":function(container,depth0,helpers,partials,
 templates['recommendation_item'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<tr>\n  <td>"
+  return "<tr>\n  <td><a href='/courses/?course_number="
     + alias4(((helper = (helper = helpers.course_numbers || (depth0 != null ? depth0.course_numbers : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"course_numbers","hash":{},"data":data}) : helper)))
-    + "</td>\n  <td>"
+    + "'>"
+    + alias4(((helper = (helper = helpers.course_numbers || (depth0 != null ? depth0.course_numbers : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"course_numbers","hash":{},"data":data}) : helper)))
+    + "</a></td>\n  <td>"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "</td>\n  <td>"
     + alias4(((helper = (helper = helpers.units || (depth0 != null ? depth0.units : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"units","hash":{},"data":data}) : helper)))
     + "</td>\n  <td>"
     + alias4(((helper = (helper = helpers.ratings || (depth0 != null ? depth0.ratings : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ratings","hash":{},"data":data}) : helper)))
-    + "</td>\n</tr>";
+    + "</td>\n</tr>\n";
 },"useData":true});
 templates['recommendations'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1;
