edb0cb691d4da66a1daedee9db706ccb5cf99d72
Lara Timbo <larat@mit.edu>
2016-11-22 23:50:23 -0500

    add handlebar recommendation views

--- public/js/recommendations.js
+++ public/js/recommendations.js
@@ -1,30 +1,30 @@
-var getCourseRecommendations = function(callback) {
-	$.get("/recommendations/courses", function(res) {
-		if (res.success) {
-			callback(res.courses);
-		}
-	})
-}
+$(function() {
+	Handlebars.partials = Handlebars.templates;
 
-var populateRecommendations = function(recs) {
-	var output = "<tr>\n<th>Course #</th>\n<th>Course Name</th>\n<th>Units</th>\n<th>Ratings</th>\n</tr>\n";
-	var addRecommendation = function(rec) {
-		output += "<tr>\n<td>" + rec.course_numbers + "</td>\n<td>" + rec.name + "</td>\n<td>" + rec.units + "</td>\n<td>3</td>\n</tr>\n";
+	var getCourseRecommendations = function(callback) {
+		$.get("/recommendations/courses", function(res) {
+			if (res.success) {
+				callback(res.courses);
+			}
+		})
 	}
-	each(recs, addRecommendation);
-	$("#recommendations").html(output);
-}
 
-var updateRecommendations = function() {
-	getCourseRecommendations(populateRecommendations);
-}
+	var populateRecommendations = function(recs) {
+		var html = Handlebars.templates.recommendations({ recommendations: recs });
+		$("#recommendations").html(html);
+	}
 
-var onLoad = function() {
-    updateRecommendations();
-}
+	var updateRecommendations = function() {
+		getCourseRecommendations(populateRecommendations);
+	}
 
-if (document.readyState != 'loading'){
-	onLoad();
-} else {
-	document.addEventListener('DOMContentLoaded', onLoad);
-}
+	var onLoad = function() {
+	   updateRecommendations();
+	}
+
+	if (document.readyState != 'loading'){
+		onLoad();
+	} else {
+		document.addEventListener('DOMContentLoaded', onLoad);
+	}
+});
