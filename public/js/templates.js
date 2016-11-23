892ab1d361f5c9c99e3b6304065ef55fcb228807
Jared Hanson <hansonj@mit.edu>
2016-11-23 06:20:01 -0500

    Added link from search page to course page

--- public/js/templates.js
+++ public/js/templates.js
@@ -31,9 +31,11 @@ templates['course_stats'] = template({"compiler":[7,">= 4.0.0"],"main":function(
 templates['courses_table_item'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<tr>\n    <td>"
+  return "<tr>\n    <td><a href='/courses/?course_number="
     + alias4(((helper = (helper = helpers.course_numbers || (depth0 != null ? depth0.course_numbers : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"course_numbers","hash":{},"data":data}) : helper)))
-    + "</td>\n    <td>"
+    + "'>"
+    + alias4(((helper = (helper = helpers.course_numbers || (depth0 != null ? depth0.course_numbers : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"course_numbers","hash":{},"data":data}) : helper)))
+    + "</a></td>\n    <td>"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
     + "</td>\n    <td value="
     + alias4(((helper = (helper = helpers.total_units || (depth0 != null ? depth0.total_units : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"total_units","hash":{},"data":data}) : helper)))
@@ -116,7 +118,7 @@ templates['recommendations'] = template({"1":function(container,depth0,helpers,p
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.recommendations : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"usePartial":true,"useData":true});
 templates['review_form'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<h1>Course Review Form</h1>\n<table>\n<tr>\n  <th>Course # Search: </th>\n  <td><input type=\"text\" id=\"course-num\"></td>\n</tr>\n</table>\n<button id=\"find-course\" class=\"btn btn-default\">Find Course</button>\n<br><br>\n<div id=\"selected-course\"><b>Course Selected:</b> None</div>\n<h5>Please answer some questions about the taking the class.</h5>\n<table>\n<tr>\n  <td>Spring/Fall/Summer/IAP?</td>\n  <td><input type=\"text\" id=\"term\"></td>\n</tr>\n<tr>\n  <td>Year?</td>\n  <td><input type=\"text\" id=\"year\"></td>\n</tr>\n<tr>\n  <td>Class Hours?</td>\n  <td><input type=\"text\" id=\"class_hrs\"></td>\n</tr>\n<tr>\n  <td>Outside Hours?</td>\n  <td><input type=\"text\" id=\"outside_hrs\"></td>\n</tr>\n<tr>\n  <td>Content Difficulty?</td>\n  <td><input type=\"text\" id=\"content_difficulty\"></td>\n</tr>\n<tr>\n  <td>Grading Difficulty?</td>\n  <td><input type=\"text\" id=\"grading_difficulty\"></td>\n</tr>\n<tr>\n  <td>Overall Satisfaction?</td>\n  <td><input type=\"text\" id=\"overall_satisfaction\"></td>\n</tr>\n</table>\n<button id=\"submit-review\" class=\"btn btn-default\">Review Course</button>\n";
+    return "<h1>Course Review Form</h1>\n<table>\n<tr>\n  <th>Course # Search: </th>\n  <td><input type=\"text\" id=\"course-num\"></td>\n</tr>\n</table>\n<button id=\"find-course\" class=\"btn btn-default\">Find Course</button>\n<br><br>\n<div id=\"selected-course\"><b>Course Selected:</b> None</div>\n<h5>Please answer some questions about the taking the class.</h5>\n<table>\n<tr>\n  <td>Spring/Fall/Summer/IAP?</td>\n  <td><input type=\"text\" id=\"term\"></td>\n</tr>\n<tr>\n  <td>Year?</td>\n  <td><input type=\"text\" id=\"year\"></td>\n</tr>\n<tr>\n  <td>Class Hours? (1-7)</td>\n  <td><input type=\"text\" id=\"class_hrs\"></td>\n</tr>\n<tr>\n  <td>Outside Hours? (1-7)</td>\n  <td><input type=\"text\" id=\"outside_hrs\"></td>\n</tr>\n<tr>\n  <td>Content Difficulty? (1-7)</td>\n  <td><input type=\"text\" id=\"content_difficulty\"></td>\n</tr>\n<tr>\n  <td>Grading Difficulty? (1-7)</td>\n  <td><input type=\"text\" id=\"grading_difficulty\"></td>\n</tr>\n<tr>\n  <td>Overall Satisfaction? (1-7)</td>\n  <td><input type=\"text\" id=\"overall_satisfaction\"></td>\n</tr>\n</table>\n<button id=\"submit-review\" class=\"btn btn-default\">Review Course</button>\n";
 },"useData":true});
 templates['review_thumbnail'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression;
