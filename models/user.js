6fbb9142944e38a35ec7acc457feab85079957db
Lara Timbo <larat@mit.edu>
2016-12-11 16:08:52 -0500

    add salting and hashing to passwords

--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
-
 var mongooseQ = require('mongoose-q')(mongoose);
 var deepPopulate = require('mongoose-deep-populate')(mongoose);
+var bcrypt = require('bcrypt');
 
 // Other models users depends on
 var Review = require('./review.js');
 var Comment = require('./comment.js');
 var Course = require('./course.js');
+
+var SALT_ROUNDS = 10;
 
 userSchema = mongoose.Schema ({
     first_name: {type: String, maxlength: 30},
@@ -25,14 +27,29 @@ userSchema = mongoose.Schema ({
     wishlist: [{type: mongoose.Schema.ObjectId, ref: 'Course'}]
 });
 
+/** hashes password */
+userSchema.pre('save', function(next) {
+	var user = this;
+	bcrypt.hash(user.password, SALT_ROUNDS).then(function(hash) {
+		user.password = hash;
+		next();
+	});
+});
+
+/** generates authentication token */
+userSchema.pre('save', function(next) {
+	var user = this;
+	user.token = generateToken();
+	next();
+});
 
 /**
  * authenticates an user
- * @param  {String} pass user's password
- * @return {boolean}      true, is passwords match. false, otherwise.
+ * @param  {String} pass user's plaintest password
+ * @return {boolean}     true, if authentication succeds, false otherwise
  */
 userSchema.methods.authenticate = function(pass) {
-  return pass === this.password && this.activated;
+	return bcrypt.compareSync(pass, this.password);
 };
 
 /**
@@ -41,35 +58,37 @@ userSchema.methods.authenticate = function(pass) {
  * @return {JSON or Boolean} json user object, or false if user_id doesn't exist
  */
 userSchema.statics.get_profile = function(user_id) {
-    return this.findById(user_id).populate('wishlist')
-         .execQ();
+	return this.findById(user_id).populate('wishlist')
+			.execQ();
 };
 
 userSchema.statics.edit_profile = function(user_id, modified){
-    return this.findOneAndUpdate({'_id': user_id}, modified)
-        .execQ();
+	return this.findOneAndUpdate({'_id': user_id}, modified)
+			.execQ();
 };
 
 userSchema.statics.post_review = function(review_form, cb){
-  var new_review = new Review(review_form);
-  var that = this;
-  return new_review.save(function(err, result){
-      if (err){
-          console.log(err);
-          return false;
-      }
-      return result;
-  });
+	var new_review = new Review(review_form);
+	var that = this;
+	return new_review.save(function(err, result){
+		if (err){
+			return false;
+		}
+		return result;
+	});
 };
 
-userSchema.statics.generateToken = function() {
-  var chars = "_!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  var token = new Date().getTime() + '_';
-  for ( var x = 0; x < 16; x++ ) {
-    var i = Math.floor( Math.random() * 62 );
-    token += chars.charAt( i );
-  }
-  return token;
+/**
+ * generates a random token
+ */
+var generateToken = function() {
+	var chars = "_!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	var token = new Date().getTime() + '_';
+	for ( var x = 0; x < 16; x++ ) {
+		var i = Math.floor( Math.random() * 62 );
+		token += chars.charAt( i );
+	}
+	return token;
 };
 
 userSchema.plugin(deepPopulate);
