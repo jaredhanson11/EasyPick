6d946b2f81ad84f6a2fb47b1789da6d368a61fb5
Jared Hanson <hansonj@mit.edu>
2016-12-12 06:31:43 -0500

    No need for hased user id anymore

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 commentSchema = mongoose.Schema ({
     content: {type: String},
     course: {type: mongoose.Schema.ObjectId, ref: 'Course'},
-    hashed_user: {type: Number} // This is the hashed user id
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
