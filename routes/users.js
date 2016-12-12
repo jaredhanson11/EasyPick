1903b0cd6ebbc3fe421f6b6f0e8aee94fff41cb9
Famien Koko <fakoko@mit.edu>
2016-12-12 09:26:03 -0500

    add comments to users.js

--- routes/users.js
+++ routes/users.js
@@ -4,26 +4,32 @@ var utils = require('../utils.js');
 
 var UsersController = require('../controllers/UsersController');
 
+/** get route for user profile page */
 router.get('/', utils.auth, function(req, res) {
   UsersController.getProfile(req, res);
 });
 
+/** put route for editing user profile */
 router.put('/', utils.auth, function(req, res){
     UsersController.editProfile(req, res);
 })
 
+/** post route for creating a review for a class */
 router.post('/review', utils.auth, function(req, res) {
     UsersController.postReview(req, res);
 });
 
+/** put route for editing a review */
 router.put('/review', utils.auth, function(req, res) {
     UsersController.editReview(req, res);
 });
 
+/** post route for adding a class to a user's wishlist */
 router.post('/wishlist', utils.auth, function(req, res){
     UsersController.postToWishlist(req, res);
 });
 
+/** delete route for removing a class from a user's wishlist */
 router.delete('/wishlist', utils.auth, function(req, res){
     UsersController.removeFromWishlist(req, res);
 });
